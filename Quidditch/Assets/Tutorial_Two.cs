0163f18a5e62fcf9ec9c8252946b642c7c2e868a
Roosh <rushilvreddy@gmail.com>
2016-07-14 16:02:48 -0700

    CurvedUI

---
+++ Quidditch/Assets/Tutorial_Two.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial_Two : MonoBehaviour {
+
+    public Vector3[] points;
+    public int Spotnum = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
