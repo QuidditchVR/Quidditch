7076f4588c2f742cc64475f6d46615fa5e44c2d5
Ken Watanabe <ken45000@gmail.com>
2016-07-14 17:56:33 -0700

    transition scene

---
+++ Quidditch/Assets/Components/EnemyManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour {
+
+    public Text scoreText;
+    public GameObject sceneRings;
+    private List<Enemy> enemies;
+    private float checkEnemyInterval = 0.5f;
+    private int originalEnemyCount;
+    private int enemyCount;
+
+	// Use this for initialization
+	void Start () {
+        var enemyObjs = GameObject.FindGameObjectsWithTag("Enemy");
+        enemies = new List<Enemy>();
+        foreach(var obj in enemyObjs) {
+            var enemy = obj.GetComponent<Enemy>();
+            if (enemy) {
+                enemies.Add(enemy);
+            }
+        }
+        enemyCount = getAliveEnemyCount();
+        originalEnemyCount = enemyCount;
+        StartCoroutine("checkEnemyCount");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    IEnumerator checkEnemyCount() {
+        while(true) {
+            enemyCount = getAliveEnemyCount();
+            if (scoreText) {
+                scoreText.text = enemyCount + "/" + originalEnemyCount;
+            }
+            if (enemyCount == 0) {
+                sceneRings.SetActive(true);
+            }
+            yield return new WaitForSeconds(checkEnemyInterval);
+        }
+    }
+
+    private int getAliveEnemyCount() {
+        int count = 0;
+        foreach (var enemy in enemies) {
+            if (enemy) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ Quidditch/Assets/Components/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+
+    public static string[] sceneNames = {
+        "Level 3",
+    };
+    public static int currentScene = 0;
+ 
+    void OnTriggerEnter(Collider c) {
+        if (c.CompareTag("Player")) {
+            currentScene++;
+            if (currentScene >= sceneNames.Length) {
+                currentScene = 0;
+            }
+            SceneManager.LoadScene(sceneNames[currentScene]);
+        }
+    }
+}
